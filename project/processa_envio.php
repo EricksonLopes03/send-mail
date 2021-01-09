1269ae4e740b71aa6c645c7852596136ef5ca24c
EricksonLopes03 <ericklsantos3@gmail.com>
2021-01-08 23:08:55 -0300

    Enviando email com os dados recebidos do frontend

--- project/processa_envio.php
+++ project/processa_envio.php
@@ -53,35 +53,35 @@
 
     //instanciando classe PHPMailer e setando configurações iniciais
     $mail = new PHPMailer(true);
+    
     try {
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
         $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp1.example.com';                    // Set the SMTP server to send through
+        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'user@example.com';                     // SMTP username
-        $mail->Password   = 'secret';                               // SMTP password
+        $mail->Username   = 'insira seu email'; // INSIRA O SEU EMAIL (PARA AUTENTICAR NO SMTP)
+        $mail->Password   = 'insira a senha'; // INSIRA A SENHA
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-        $mail->addAddress('ellen@example.com');               // Name is optional
-        $mail->addReplyTo('info@example.com', 'Information');
-        $mail->addCC('cc@example.com');
-        $mail->addBCC('bcc@example.com');
+        $mail->setFrom('Insira o email do remetente', 'Erickson - Remetente'); //remetente
+        $mail->addAddress($mensagem->__get('email'), 'Erickson - Destinatário');     //destinatário
+        //$mail->addAddress('ellen@example.com');               // Name is optional
+        //$mail->addReplyTo('info@example.com', 'Information'); //e-mail cujo qual a resposta do destinatário será direcionada
+        //$mail->addCC('cc@example.com'); //copia
+        //$mail->addBCC('bcc@example.com'); //copia oculta
 
-        // Attachments
-        $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+        // Attachments - anexos
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
         // Content
         $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
+        $mail->Subject = $mensagem->__get('assunto'); //assunto
+        $mail->Body    = $mensagem->__get('mensagem'); // corpo que possui suporte para tags html
+        $mail->AltBody = $mensagem->__get('mensagem'); // corpo alternativo para clientes que nao tem suporte para tags html
         $mail->send();
         echo 'Message has been sent';
     } catch (Exception $e) {
